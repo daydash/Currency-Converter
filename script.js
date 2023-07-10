a4610c2736f2842e6e808e0c1cd7263cd7fff479
daydash <thedaydash@gmail.com>
2023-07-10 11:16:03 +0530

    3_xJ9a4ujM_added_styling_and_updated_swap_currency_feature

--- script.js
+++ script.js
@@ -7,79 +7,90 @@ const outputText = document.getElementById("outputText");
 const form = document.querySelector(".form");
 const swapButton = document.getElementById("swapButton");
 
-const response = data.data;
+const getData = async () => {
+  //   const response = await fetch(
+  //     "https://api.freecurrencyapi.com/v1/latest?apikey=n9le9NlNoevcE4LjQrtxCRw9IFddoqVzGzyH0cV7"
+  //   );
+  //   const data = await response.json();
+  const currencyData = await data.data;
 
-const options = [];
-Object.keys(response).forEach((key) => {
-	options.push(key);
-});
+  const options = [];
+  Object.keys(currencyData).forEach((key) => {
+    options.push(key);
+  });
 
-// Object.entries(response).forEach((entry) => {
-// 	const [key] = entry;
-// 	options.push(key);
-// });
+  // Object.entries(currencyData).forEach((entry) => {
+  // 	const [key] = entry;
+  // 	options.push(key);
+  // });
 
-options.map((key) => {
-	const optionsList = document.createElement("option");
-	optionsList.value = key;
-	optionsList.innerText = key;
-	inputCurrency.appendChild(optionsList);
-});
-options.map((key) => {
-	const optionsList = document.createElement("option");
-	optionsList.value = key;
-	optionsList.innerText = key;
-	outputCurrency.appendChild(optionsList);
-});
+  options.map((key) => {
+    const optionsList = document.createElement("option");
+    optionsList.value = key;
+    optionsList.innerText = key;
+    optionsList.classList.add("options");
+    inputCurrency.appendChild(optionsList);
+  });
+  options.map((key) => {
+    const optionsList = document.createElement("option");
+    optionsList.value = key;
+    optionsList.innerText = key;
+    optionsList.classList.add("options");
+    outputCurrency.appendChild(optionsList);
+  });
 
-// Restricts input for the given textbox to the given inputFilter.
-function setInputFilter(textbox, inputFilter) {
-	[
-		"input",
-		"keydown",
-		"keyup",
-		"mousedown",
-		"mouseup",
-		"select",
-		"contextmenu",
-		"drop",
-	].forEach(function (event) {
-		textbox.addEventListener(event, function () {
-			if (inputFilter(this.value)) {
-				this.oldValue = this.value;
-				this.oldSelectionStart = this.selectionStart;
-				this.oldSelectionEnd = this.selectionEnd;
-			} else if (this.hasOwnProperty("oldValue")) {
-				this.value = this.oldValue;
-				this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
-			}
-		});
-	});
-}
+  // Restricts input for the given textbox to the given inputFilter.
+  function setInputFilter(textbox, inputFilter) {
+    [
+      "input",
+      "keydown",
+      "keyup",
+      "mousedown",
+      "mouseup",
+      "select",
+      "contextmenu",
+      "drop",
+    ].forEach(function (event) {
+      textbox.addEventListener(event, function () {
+        if (inputFilter(this.value)) {
+          this.oldValue = this.value;
+          this.oldSelectionStart = this.selectionStart;
+          this.oldSelectionEnd = this.selectionEnd;
+        } else if (this.hasOwnProperty("oldValue")) {
+          this.value = this.oldValue;
+          this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+        }
+      });
+    });
+  }
 
-setInputFilter(inputText, function (value) {
-	return /^\d*\.?\d*$/.test(value);
-});
-setInputFilter(outputText, function (value) {
-	return /^\d*\.?\d*$/.test(value);
-});
+  setInputFilter(inputText, function (value) {
+    return /^\d*\.?\d*$/.test(value);
+  });
+  setInputFilter(outputText, function (value) {
+    return /^\d*\.?\d*$/.test(value);
+  });
 
-form.addEventListener("submit", (e) => {
-	e.preventDefault();
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-	const inputValue = inputText.value;
-	const inputOption = inputCurrency.value;
-	const outputOption = outputCurrency.value;
-	const outputValue =
-		(inputValue * response[outputOption]) / response[inputOption];
+    const inputValue = inputText.value;
+    const inputOption = inputCurrency.value;
+    const outputOption = outputCurrency.value;
+    const outputValue =
+      (inputValue * currencyData[outputOption]) / currencyData[inputOption];
 
-	outputText.value = outputValue;
-});
+    outputText.value = outputValue;
+  });
 
-swapButton.addEventListener("click", () => {
-	const temp = outputCurrency.value;
-	outputCurrency.value = inputCurrency.value;
-	inputCurrency.value = temp;
-});
+  swapButton.addEventListener("click", () => {
+    let temp = outputCurrency.value;
+    outputCurrency.value = inputCurrency.value;
+    inputCurrency.value = temp;
 
-
+    temp = inputText.value;
+    inputText.value = outputText.value;
+    outputText.value = temp;
+  });
+};
+getData();
