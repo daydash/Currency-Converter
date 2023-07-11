d63fb0497967b753d7437fe71c2aec681ec3267b
daydash <thedaydash@gmail.com>
2023-07-11 12:54:46 +0530

    4_xJ9a4ujM_added_feedbacks_to_illegal_options

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ const inputText = document.getElementById("inputText");
 const outputText = document.getElementById("outputText");
 const form = document.querySelector(".form");
 const swapButton = document.getElementById("swapButton");
+const convertBtn = document.getElementById("convertBtn");
 
 const getData = async () => {
-  //   const response = await fetch(
-  //     "https://api.freecurrencyapi.com/v1/latest?apikey=n9le9NlNoevcE4LjQrtxCRw9IFddoqVzGzyH0cV7"
-  //   );
-  //   const data = await response.json();
+  // const response = await fetch(
+  //   "https://api.freecurrencyapi.com/v1/latest?apikey=n9le9NlNoevcE4LjQrtxCRw9IFddoqVzGzyH0cV7"
+  // );
+  // const data = await response.json();
   const currencyData = await data.data;
 
   const options = [];
@@ -50,7 +51,7 @@ const getData = async () => {
       "select",
       "contextmenu",
       "drop",
-    ].forEach(function (event) {
+    ].forEach((event) => {
       textbox.addEventListener(event, function () {
         if (inputFilter(this.value)) {
           this.oldValue = this.value;
@@ -67,30 +68,79 @@ const getData = async () => {
   setInputFilter(inputText, (value) => {
     return /^\d*\.?\d*$/.test(value);
   });
-  setInputFilter(outputText, (value) => {
-    return /^\d*\.?\d*$/.test(value);
+  // setInputFilter(outputText, (value) => {
+  //   return /^\d*\.?\d*$/.test(value);
+  // });
+
+  inputText.addEventListener("input", () => {
+    inputText.classList.remove("redBorder");
+  });
+
+  inputCurrency.addEventListener("input", () => {
+    inputCurrency.classList.remove("redBorder");
+    outputCurrency.classList.remove("redBorder");
+  });
+
+  outputCurrency.addEventListener("input", () => {
+    inputCurrency.classList.remove("redBorder");
+    outputCurrency.classList.remove("redBorder");
   });
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-
     const inputValue = inputText.value;
     const inputOption = inputCurrency.value;
     const outputOption = outputCurrency.value;
-    const outputValue =
-      (inputValue * currencyData[outputOption]) / currencyData[inputOption];
 
-    outputText.value = outputValue;
+    if (inputValue.trim() !== "") {
+      inputText.classList.remove("redBorder");
+      if (inputOption !== outputOption) {
+        outputCurrency.classList.remove("redBorder");
+        inputCurrency.classList.remove("redBorder");
+        convertBtn.classList.remove("sameCurrencyErrorBtn");
+
+        const outputValue =
+          (inputValue * currencyData[outputOption]) / currencyData[inputOption];
+
+        outputText.value = outputValue;
+      } else {
+        outputText.value = "";
+        outputCurrency.classList.add("redBorder");
+        outputCurrency.focus();
+        inputCurrency.classList.add("redBorder");
+        inputCurrency.focus();
+      }
+    } else {
+      convertBtn.classList.add("sameCurrencyErrorBtn");
+      setTimeout(() => {
+        convertBtn.classList.remove("sameCurrencyErrorBtn");
+      }, 1500);
+      inputText.classList.add("redBorder");
+      inputText.focus();
+    }
   });
 
   swapButton.addEventListener("click", () => {
-    let temp = outputCurrency.value;
-    outputCurrency.value = inputCurrency.value;
-    inputCurrency.value = temp;
+    const inputValue = inputText.value;
+    const outputValue = inputText.value;
+    const inputOption = inputCurrency.value;
+    const outputOption = outputCurrency.value;
 
-    temp = inputText.value;
-    inputText.value = outputText.value;
-    outputText.value = temp;
+    if (inputOption !== outputOption) {
+      let temp = outputOption;
+      outputOption = inputOption;
+      inputOption = temp;
+    } else {
+      outputCurrency.classList.add("redBorder");
+      outputCurrency.focus();
+      inputCurrency.classList.add("redBorder");
+      inputCurrency.focus();
+    }
+    if (outputValue.trim() !== "") {
+      temp = inputValue;
+      inputValue = outputValue;
+      outputValue = temp;
+    }
   });
 };
 getData();
