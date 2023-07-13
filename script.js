7044dbd0413c8146667f173b9b8246db72ff1956
daydash <thedaydash@gmail.com>
2023-07-13 11:43:22 +0530

    6_xJ9a4ujM_optimised_regex_function

--- script.js
+++ script.js
@@ -42,40 +42,56 @@ const getData = async () => {
     outputCurrency.appendChild(optionsList);
   });
 
-  console.log("test");
   // Restricts input for the given textbox to the given inputFilter.
-  function setInputFilter(textbox, inputFilter) {
-    [
-      "input",
-      "keydown",
-      "keyup",
-      "mousedown",
-      "mouseup",
-      "select",
-      "contextmenu",
-      "drop",
-    ].forEach((event) => {
-      textbox.addEventListener(event, function () {
-        if (inputFilter(this.value)) {
-          this.oldValue = this.value;
-          this.oldSelectionStart = this.selectionStart;
-          this.oldSelectionEnd = this.selectionEnd;
-        } else if (this.hasOwnProperty("oldValue")) {
-          this.value = this.oldValue;
-          this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
-        }
-      });
-    });
-  }
+  // function setInputFilter(textbox, inputFilter) {
+  //   [
+  //     "input",
+  //     "keydown",
+  //     "keyup",
+  //     "mousedown",
+  //     "mouseup",
+  //     "select",
+  //     "contextmenu",
+  //     "drop",
+  //   ].forEach((event) => {
+  //     console.log("after for each");
+  //     textbox.addEventListener(event, function () {
+  //       console.log("after eventlistener");
+  //       if (inputFilter(this.value)) {
+  //         this.oldValue = this.value;
+  //         this.oldSelectionStart = this.selectionStart;
+  //         this.oldSelectionEnd = this.selectionEnd;
+  //       } else if (this.hasOwnProperty("oldValue")) {
+  //         this.value = this.oldValue;
+  //         this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+  //       }
+  //     });
+  //   });
+  // }
 
-  setInputFilter(inputText, (value) => {
-    console.log("I am");
+  // setInputFilter(inputText, (value) => {
+  //   return /^\d*\.?\d*$/.test(value);
+  // });
+
+  const inputFilter = (value) => {
     return /^\d*\.?\d*$/.test(value);
-  });
+  };
 
-  console.log(setInputFilter);
+  inputText.addEventListener("input", (e) => {
+    if (inputFilter(e.target.value)) {
+      e.target.oldValue = e.target.value;
+      e.target.oldSelectionStart = e.target.selectionStart;
+      e.target.oldSelectionEnd = e.target.selectionEnd;
+    } else if (e.target.hasOwnProperty("oldValue")) {
+      e.target.value = e.target.oldValue;
+      e.target.setSelectionRange(
+        e.target.oldSelectionStart,
+        e.target.oldSelectionEnd
+      );
+    } else {
+      e.target.value = "";
+    }
 
-  inputText.addEventListener("input", () => {
     inputText.classList.remove("redBorder");
     inputValueError.classList.remove("warning");
   });
